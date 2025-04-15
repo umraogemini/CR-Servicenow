a743350b2e5ba6a02247c50633b770b7deecb7f2
umraogemini <umraogemini@gmail.com>
2025-04-15 20:13:31 +0530

    Update output.tf

--- modules/servicenow-cr/output.tf
+++ modules/servicenow-cr/output.tf
@@ -1,7 +1,9 @@
 output "automation_ci_sys_id" {
   value = servicenow_ci.automation_creation.sys_id
+  description = "The sys_id of the created Automation CI."
 }
 
 output "automation_ci_number" {
   value = servicenow_ci.automation_creation.number
+  description = "The number of the created Automation CI."
 }
